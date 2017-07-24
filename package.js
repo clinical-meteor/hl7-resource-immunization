00b113f631788742940ef96b5e0264350d297c91
Abigail Watson <awatson1978@gmail.com>
2017-07-23 19:21:28 -0500

    defaultValues

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-immunization',
-  version: '1.0.3',
+  version: '1.0.5',
   summary: 'HL7 FHIR Resource - Immunization',
   git: 'https://github.com/clinical-meteor/hl7-resource-immunization',
   documentation: 'README.md'
@@ -14,10 +14,10 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.6.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:hl7-resource-bundle@1.3.10');
 
   api.addFiles('lib/hl7-resource-immunization.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
